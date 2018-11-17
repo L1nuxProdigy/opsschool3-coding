02159fa8b1c4d713427046780e3fee3bec979227
Nadav <nadavops@protonmail.com>
2018-11-17 20:57:38 +0200

    Denied once, made some changes- not all

--- home-assignments/session2/clickEx.py
+++ home-assignments/session2/clickEx.py
@@ -10,38 +10,32 @@ except ModuleNotFoundError:
 import click
 from weather import Weather, Unit
 
+
 @click.command()
 @click.option('--city', default="Error", help='The Desired City')
 @click.option('--forecast', default="TODAY", help='day/s of forecast')
-@click.option('-c/-f', default=True, help='temparture in celsius or fahrenheit')
+@click.option('-c/-f', default=True, help='temperature in celsius or fahrenheit')
+
 
 def main(city, forecast, c):
-    check_city_name(city)
     check_forecast_input(forecast)
     temperature_metric, weather = what_temperature_metric(c)
     location, condition = check_valid_location(city, weather)
     if forecast == "TODAY":
-        low_temperature, high_temperature = output_for_single_day(forecast, location)
-        print("The Weather in {} today is {} with temperatures trailing from {}-{} {}".format(city, condition.text,
-                                                                                              low_temperature,
-                                                                                              high_temperature,
-                                                                                              temperature_metric))
+        output_for_single_day(city,location,condition,temperature_metric)
     else:
+        output_for_single_day(city, location, condition, temperature_metric)
         output_for_multiple_days(forecast, location, temperature_metric)
 
 
-
-def check_city_name(city):
-    if city == "Error":
-        print("use the --city flag followed by a city name")
-        sys.exit()
 
 def check_forecast_input(forecast):
     if forecast[:5] != "TODAY":
         print("Input incorrect!, forecast parameter should start with TODAY")
         sys.exit()
     if len(forecast) == 6 and forecast[5] != "+":
-        print("Input incorrect!, forecast parameter should start with TODAY, TODAY+int can be used to receive upcoming forecasts")
+        print(
+            "Input incorrect!, forecast parameter should start with TODAY, TODAY+int can be used to receive upcoming forecasts")
         sys.exit()
     if len(forecast) <= 5:
         return
@@ -54,22 +48,26 @@ def check_forecast_input(forecast):
             print("dude stop trying to mess with the program and enter somthing valid")
             sys.exit()
 
+
 def weather_in_fahrenheit():
     weather = Weather(unit=Unit.FAHRENHEIT)
     return weather
+
 
 def weather_in_celsius():
     weather = Weather(unit=Unit.CELSIUS)
     return weather
 
+
 def what_temperature_metric(c):
     if c:
         weather = weather_in_celsius()
-        temparture_metric = "celsius"
+        temperature_metric = "celsius"
     else:
         weather = weather_in_fahrenheit()
-        temparture_metric = "fahrenheit"
-    return temparture_metric, weather
+        temperature_metric = "fahrenheit"
+    return temperature_metric, weather
+
 
 def check_valid_location(city, weather):
     location = weather.lookup_by_location(city)
@@ -80,9 +78,16 @@ def check_valid_location(city, weather):
         sys.exit()
     return location, condition
 
-def output_for_single_day(forecast, location):
+
+def output_for_single_day(city,location,condition,temperature_metric):
     forecasts = location.forecast
-    return forecasts[0].low, forecasts[0].high
+    low_temperature = forecasts[0].low
+    high_temperature = forecasts[0].high
+    print("The Weather in {} today is {} with temperatures trailing from {}-{} {}".format(city, condition.text,
+                                                                                          low_temperature,
+                                                                                          high_temperature,
+                                                                                          temperature_metric))
+
 
 def output_for_multiple_days(forecast, location, temperature_metric):
     days_after = forecast[-1]
@@ -95,7 +100,6 @@ def output_for_multiple_days(forecast, location, temperature_metric):
         high_temperature = forecasts[forecast_index].high
         print("{} {} with temperatures trailing from {}-{} {}.".format(date, location_overview, low_temperature,
                                                                        high_temperature, temperature_metric))
-
 
 
 if __name__ == "__main__":
